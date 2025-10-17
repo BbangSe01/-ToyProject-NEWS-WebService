eecd3a9acb1f3d5dff8e542f1e1d5d4734256902
bangsehyeon <bangsehyeon7091@gmail.com>
2025-10-17 15:56:35 +0900

    [CHORE]main.ts에 Bootstrap 적용

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Bootstrap JS (bundle: collapse, dropdown 등 포함)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+createApp(App).mount('#app')
