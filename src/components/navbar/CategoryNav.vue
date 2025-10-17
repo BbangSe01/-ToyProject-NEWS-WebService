d8a2bf963f1f69a5dcb5f7d541a96b31c7758938
bangsehyeon <bangsehyeon7091@gmail.com>
2025-10-17 16:09:35 +0900

    [FEAT]카테고리 드롭다운 생성

---
+++ src/components/navbar/CategoryNav.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="dropdown">
+        <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+        >
+            {{ categories[categoryIdx] }}
+        </button>
+        <ul class="dropdown-menu">
+            <li
+                v-for="(category, index) in categories"
+                :key="index"
+                v-on:click="clickCategory(index)"
+            ><a
+                    class="dropdown-item"
+                    href="#"
+                >{{ category }}</a></li>
+        </ul>
+    </div>
+
+</template>
+
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const categories: string[] = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+const categoryIdx = ref<number>(0);
+
+
+const clickCategory = (index: number) => {
+    categoryIdx.value = index;
+}
+</script>
+<style></style>
