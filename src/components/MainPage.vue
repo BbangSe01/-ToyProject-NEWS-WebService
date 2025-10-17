b84236aae9aebfd4ceb4f8eb73149e6a21b6aec6
bangsehyeon <bangsehyeon7091@gmail.com>
2025-10-17 16:08:19 +0900

    [FEAT] 메인페이지 생성 및 API 호출 테스트

---
+++ src/components/MainPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <CategoryNav class="categoryBtn" />
+</template>
+
+<script setup lang="ts">
+import CategoryNav from './navbar/CategoryNav.vue';
+import { getHeadLineData } from '../apis/NewsApis';
+
+getHeadLineData();
+</script>
+
+
+<style>
+.read-the-docs {
+  color: #888;
+}
+
+.categoryBtn {
+  margin-top: 1rem;
+  margin-left: 1rem;
+}
+</style>
