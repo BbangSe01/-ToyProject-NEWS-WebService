8b9a676dcd831747f71a7ff72e54165827482504
bangsehyeon <tpwnstpgus1@naver.com>
2025-11-04 15:33:58 +0900

    [CHORE] headline 받아오는 데이터 개수 수정

--- src/apis/NewsApis.ts
+++ src/apis/NewsApis.ts
@@ -1,13 +1,14 @@
-import axiosInstance from "./axiosInstance"
+import axiosInstance from "./axiosInstance";
 
-const apikey = import.meta.env.VITE_NEWS_API_KEY
+const apikey = import.meta.env.VITE_NEWS_API_KEY;
 
-export const getHeadLineData = async (category:string) => {
-    try {
-        const res = await axiosInstance.get(`top-headlines?country=us&pageSize=10&category=${category}&apikey=${apikey}`)
-        return res.data.articles;
-    } catch(err) {
-        console.error(err);
-    }
-    
-}
+export const getHeadLineData = async (category: string) => {
+  try {
+    const res = await axiosInstance.get(
+      `top-headlines?country=us&pageSize=7&category=${category}&apikey=${apikey}`
+    );
+    return res.data.articles;
+  } catch (err) {
+    console.error(err);
+  }
+};
