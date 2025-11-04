a2b5843603ac191160d195c0eb30d3b03f2c8573
bangsehyeon <tpwnstpgus1@naver.com>
2025-11-04 10:53:18 +0900

    [FEAT] v-for문을 통해 Card컴포넌트에 newsData 전달

--- src/components/templates/HeadLine.vue
+++ src/components/templates/HeadLine.vue
@@ -1,36 +1,50 @@
 <template>
-    <CategoryNav
-        :categories="categories"
-        @update-category="categories.index = $event"
-        class="categoryBtn"
-    />
+  <CategoryNav
+    :categories="categories"
+    @update-category="categories.index = $event"
+    class="categoryBtn"
+  />
+  <div v-for="item in headlineData" key="item.title">
+    <NewsCard :newsData="item" />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { reactive, watch, ref } from 'vue';
-import CategoryNav from '../navbar/CategoryNav.vue';
-import type { CategoryType, NewsType } from '../../types';
-import { getHeadLineData } from '../../apis/NewsApis';
+  import { reactive, watch, ref } from "vue";
+  import NewsCard from "../parts/NewsCard.vue";
+  import CategoryNav from "../navbar/CategoryNav.vue";
+  import type { CategoryType, NewsType } from "../../types";
+  import { getHeadLineData } from "../../apis/NewsApis";
 
-const categories = reactive<CategoryType>({
-    list: ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'],
-    index: 0
-})
+  const categories = reactive<CategoryType>({
+    list: [
+      "business",
+      "entertainment",
+      "general",
+      "health",
+      "science",
+      "sports",
+      "technology",
+    ],
+    index: 0,
+  });
 
-const headlineData = ref<Array<NewsType>>([]);
-watch(() => categories.index, async (newVal: number, oldVal: number) => {
-    headlineData.value = await getHeadLineData(categories.list[newVal]!);
-})
+  const headlineData = ref<Array<NewsType>>([]);
+  watch(
+    () => categories.index,
+    async (newVal: number, oldVal: number) => {
+      headlineData.value = await getHeadLineData(categories.list[newVal]!);
+    }
+  );
 </script>
 
-
 <style>
-.read-the-docs {
+  .read-the-docs {
     color: #888;
-}
+  }
 
-.categoryBtn {
+  .categoryBtn {
     margin-top: 1rem;
     margin-left: 1rem;
-}
+  }
 </style>
