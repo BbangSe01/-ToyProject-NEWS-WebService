373b0cc26ae0515eca33ab7db7397aef13b07889
bangsehyeon <tpwnstpgus1@naver.com>
2025-11-04 16:31:20 +0900

    [FEAT] 선택한 버튼 CSS 다르게 적용

--- src/components/navbar/CategoryNav.vue
+++ src/components/navbar/CategoryNav.vue
@@ -2,7 +2,7 @@
   <div class="button-line">
     <button
       v-for="(category, idx) in props.categories.list"
-      class="each-btn"
+      :class="{ active: idx === props.categories.index }"
       v-on:click="clickCategory(idx)"
     >
       {{ category }}
@@ -19,6 +19,7 @@
   }>();
 
   console.log(props.categories);
+  console.log(props.categories);
   const emit = defineEmits(["update-category"]);
 
   const clickCategory = (index: number) => {
@@ -28,12 +29,17 @@
 <style scoped>
   .button-line {
     display: flex;
-    margin-bottom: 3rem;
+    margin-bottom: 2rem;
   }
-  .each-btn {
+  button {
     background-color: white;
     margin-right: 1rem;
     border: 1px solid black;
     border-radius: 10px;
+  }
+  button.active {
+    background-color: black;
+    color: white;
+    border-color: white;
   }
 </style>
