a5faac02345a09d62ba2b86e8b41dec4373cc457
bangsehyeon <tpwnstpgus1@naver.com>
2025-11-06 15:49:06 +0900

    [CHORE] 모달창으로 상세페이지 구현하기 위해 기존 컴포넌트 제거

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MainPage from "../components/MainPage.vue";
-import DetailNewsPage from "../components/templates/DetailNewsPage.vue";
 
-const routes = [
-  { path: "/", component: MainPage },
-  { path: "/detailNews", component: DetailNewsPage },
-];
+const routes = [{ path: "/", component: MainPage }];
 
 const router = createRouter({
   history: createWebHistory(),
