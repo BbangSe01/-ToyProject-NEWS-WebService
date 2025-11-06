2e3b393d8980c3687251aedb76fb1744558e6356
bangsehyeon <tpwnstpgus1@naver.com>
2025-11-06 14:36:52 +0900

    [CHORE] pinia store 구성

---
+++ src/stores/newsData.ts
@@ -0,0 +1,22 @@
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+import type { NewsType } from "../types/newsType";
+
+export const useNewsDataStore = defineStore("newsData", () => {
+  const detailData = ref<NewsType>({
+    author: "",
+    content: "",
+    description: "",
+    publishedAt: "",
+    source: { name: "", id: "" },
+    title: "",
+    url: "",
+    urlToImage: "",
+  });
+
+  const setDetailData = (newData: NewsType) => {
+    detailData.value = newData;
+  };
+
+  return { detailData, setDetailData };
+});
