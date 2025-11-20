038f651ce99bab080c84ac91a577bf986122317a
bangsehyeon <tpwnstpgus1@naver.com>
2025-11-20 09:51:18 +0900

    [FEAT] 로그인 페이지 vue-router 연결

---
+++ src/components/templates/LoginPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <p>로그인 페이지</p>
+</template>
+
+<script setup lang="ts">
+  console.log("로그인 페이지");
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HeadLinePage from "../components/templates/HeadLinePage.vue";
 import SearchPage from "../components/templates/SearchPage.vue";
+import LoginPage from "../components/templates/LoginPage.vue";
 const routes = [
   { path: "/", name: "Headline", component: HeadLinePage },
   { path: "/search", name: "Search", component: SearchPage },
+  { path: "/login", name: "Login", component: LoginPage },
 ];
 
 const router = createRouter({
