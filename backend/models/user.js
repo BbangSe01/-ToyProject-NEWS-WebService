a447c9a8021f55269ce61bb6d225c6e02514d14a
bangsehyeon <tpwnstpgus1@naver.com>
2025-11-20 16:55:12 +0900

    [FEAT] 사용자 스키마 생성 (Backend)

---
+++ backend/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose"); // 몽구스를 가져온다.
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
