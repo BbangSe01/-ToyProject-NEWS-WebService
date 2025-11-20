39e1e8d06ed2caed70a79a9c4f93abdb91b1b30a
bangsehyeon <tpwnstpgus1@naver.com>
2025-11-20 16:41:18 +0900

    [FEAT] 로그인/회원가입 시 사용하는 axiosInstance 생성

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,10 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = import.meta.env.VITE_NEWS_BASE_URL
+const baseURL = import.meta.env.VITE_NEWS_BASE_URL;
+const authURL = import.meta.env.VITE_AUTH_BASE_URL;
+export const axiosInstance = axios.create({
+  baseURL: baseURL,
+  timeout: 2000,
+});
 
-const axiosInstance = axios.create({
-    baseURL: baseURL,
-    timeout: 2000,
-})
-
-export default axiosInstance;
+export const authInstance = axios.create({
+  baseURL: authURL,
+  timeout: 2000,
+});
