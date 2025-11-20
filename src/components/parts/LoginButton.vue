a825597102c69537ecb7a0436dbdb2b11bd5fc98
bangsehyeon <tpwnstpgus1@naver.com>
2025-11-20 09:49:42 +0900

    [FEAT] 로그인 버튼 생성

---
+++ src/components/parts/LoginButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <router-link to="/login" class="login-button">Login</router-link>
+</template>
+
+<style>
+  .login-button {
+    font-family: "Merriweather", serif;
+    width: 4rem;
+    height: 2rem;
+    border: 1px solid white;
+    font-size: 1rem;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    margin-left: 1rem;
+    color: inherit;
+    text-decoration: none;
+  }
+  .login-button:hover {
+    background-color: white;
+    color: black;
+  }
+</style>
