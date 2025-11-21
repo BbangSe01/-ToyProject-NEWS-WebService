151ba988bef46d0615791512e8d5de803749d5cd
bangsehyeon <tpwnstpgus1@naver.com>
2025-11-21 15:11:53 +0900

    [REFACTOR] 비밀번호 확인 INPUT도 SignupForm 컴포넌트에 추가해서 관리

---
+++ src/components/parts/auth/SignupInput.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="signup-input-area">
+    <div class="username-line">
+      <input
+        :value="modelValue.username"
+        class="signup-username-input"
+        required
+        placeholder="  Email"
+        type="email"
+        @input="renewUsername"
+      />
+      <button class="dup-check-btn">중복확인</button>
+    </div>
+    <input
+      :value="modelValue.password"
+      class="signup-pw-input"
+      required
+      placeholder="  Password"
+      type="password"
+      @input="renewPassword"
+    />
+    <input
+      :value="modelValue.password2"
+      class="signup-pw-input"
+      required
+      placeholder="  Confirm Password"
+      type="password"
+      @input="renewPassword2"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { defineProps, defineEmits } from "vue";
+  import type { baseInputType } from "../../../types";
+  const props = defineProps<{
+    modelValue: baseInputType;
+  }>();
+
+  const emit = defineEmits(["update:modelValue"]);
+
+  const renewUsername = (e: Event) => {
+    const newValue = (e.target as HTMLInputElement).value;
+
+    emit("update:modelValue", {
+      ...props.modelValue,
+      username: newValue,
+    });
+  };
+
+  const renewPassword = (e: Event) => {
+    const newValue = (e.target as HTMLInputElement).value;
+
+    emit("update:modelValue", {
+      ...props.modelValue,
+      password: newValue,
+    });
+  };
+
+  const renewPassword2 = (e: Event) => {
+    const newValue = (e.target as HTMLInputElement).value;
+
+    emit("update:modelValue", {
+      ...props.modelValue,
+      password2: newValue,
+    });
+  };
+</script>
+
+<style scoped>
+  .signup-input-area {
+    display: flex;
+    flex-direction: column;
+  }
+  .username-line {
+    display: flex;
+  }
+  .signup-username-input {
+    width: 13rem;
+    height: 2rem;
+    border: 2px solid black;
+    border-radius: 10px;
+    margin-bottom: 0.5rem;
+  }
+  .signup-pw-input {
+    width: 18rem;
+    height: 2rem;
+    border: 2px solid black;
+    border-radius: 10px;
+    margin-bottom: 0.5rem;
+  }
+  .dup-check-btn {
+    width: 4.5rem;
+    margin-left: 0.5rem;
+    height: 2rem;
+    font-size: 0.8rem;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+</style>
--- src/types/baseInputType.ts
+++ src/types/baseInputType.ts
@@ -1,6 +1,7 @@
 export interface baseInputType {
   username: string;
   password: string;
+  password2: string;
 }
 
 export interface baseValidType {
