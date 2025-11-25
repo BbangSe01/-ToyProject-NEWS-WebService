0f26463dfc2b8294cc731daf74d3cf20b9489759
bangsehyeon <tpwnstpgus1@naver.com>
2025-11-25 16:21:05 +0900

    [CHORE] 회원가입 페이지에서 에러 처리 위해 메서드 try/catch 제거

--- src/apis/AuthApis.ts
+++ src/apis/AuthApis.ts
@@ -1,13 +1,8 @@
 import { authInstance } from "./axiosInstance";
 import type { SignupType } from "../types";
 export const signup = async ({ username, password }: SignupType) => {
-  try {
-    const res = await authInstance.post("user/register", {
-      username,
-      password,
-    });
-    return res;
-  } catch (err) {
-    console.error(err);
-  }
+  return await authInstance.post("user/register", {
+    username,
+    password,
+  });
 };
