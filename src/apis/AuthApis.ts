49df235db11770ca3b56058ae70feac9f88941ab
bangsehyeon <tpwnstpgus1@naver.com>
2025-11-27 17:03:12 +0900

    [CHORE] .env 상수명 변경

--- src/apis/AuthApis.ts
+++ src/apis/AuthApis.ts
@@ -1,14 +1,14 @@
-import { authInstance } from "./axiosInstance";
+import { backendInstance } from "./axiosInstance";
 import type { SignupType } from "../types";
 export const signup = async ({ username, password }: SignupType) => {
-  return await authInstance.post("user/register", {
+  return await backendInstance.post("user/register", {
     username,
     password,
   });
 };
 
 export const login = async ({ username, password }: SignupType) => {
-  return await authInstance.post("user/login", {
+  return await backendInstance.post("user/login", {
     username,
     password,
   });
--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const baseURL = import.meta.env.VITE_NEWS_BASE_URL;
-const authURL = import.meta.env.VITE_AUTH_BASE_URL;
+const backendURL = import.meta.env.VITE_BACKEND_BASE_URL;
 export const axiosInstance = axios.create({
   baseURL: baseURL,
   timeout: 2000,
 });
 
-export const authInstance = axios.create({
-  baseURL: authURL,
+export const backendInstance = axios.create({
+  baseURL: backendURL,
   timeout: 2000,
 });
