52c22054f3a2a08a38e31bb4263011a9c72eceb7
bangsehyeon <tpwnstpgus1@naver.com>
2025-11-28 16:28:17 +0900

    [FEAT] Gemini 프롬포트 템플릿 구현 (Backend)

---
+++ backend/utils/askToGemini.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const { GoogleGenAI } = require("@google/genai");
+
+const ai = new GoogleGenAI({});
+
+const askToGemini = async (content) => {
+  const prompt = `Please summarize the following article concisely:\n\n---\n\n${content}`;
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: prompt,
+  });
+  return response;
+};
+
+module.exports = askToGemini;
