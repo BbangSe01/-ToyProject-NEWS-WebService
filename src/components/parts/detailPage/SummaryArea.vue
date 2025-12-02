4a102a8be826af55290bdde761d3c9d148d42e1d
bangsehyeon <tpwnstpgus1@naver.com>
2025-12-02 15:51:31 +0900

    [FEAT] 요약문 컴포넌트 추가

---
+++ src/components/parts/detailPage/SummaryArea.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="summary-area">
+    <p class="summary-title">Summary</p>
+    <div class="summary-content">{{ props.content }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { defineProps } from "vue";
+
+  const props = defineProps<{
+    content: string;
+  }>();
+</script>
+
+<style>
+  .summary-area {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 2rem;
+  }
+  .summary-title {
+    font-weight: bold;
+    font-size: 2rem;
+  }
+  .summary-content {
+    line-height: 1.5rem;
+  }
+</style>
