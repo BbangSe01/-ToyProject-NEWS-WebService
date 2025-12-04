425dc68abc409d62153a8a794ad2bedd991e76d3
bangsehyeon <tpwnstpgus1@naver.com>
2025-12-04 11:10:42 +0900

    [FEAT] 백엔드 API 요청 시, axios interceptors에서 자동으로 토큰 header에 추가

--- src/apis/instance/TokenHttp.ts
+++ src/apis/instance/TokenHttp.ts
@@ -1,7 +1,21 @@
-import axios from "axios";
-
+import axios, { type AxiosRequestConfig } from "axios";
+import { useTokenDataStore } from "../../stores/tokenData";
 const backendURL = import.meta.env.VITE_BACKEND_BASE_URL;
 export const backendInstance = axios.create({
   baseURL: backendURL,
   timeout: 2000,
 });
+
+backendInstance.interceptors.request.use(
+  (config) => {
+    const useAuthStore = useTokenDataStore();
+    const token = useAuthStore.accessToken;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
