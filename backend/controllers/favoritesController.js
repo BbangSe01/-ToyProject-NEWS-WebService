b1b5c3eedaebb8daf2b9ef41ba8dc0c6971df0dc
bangsehyeon <bangsehyeon7091@gmail.com>
2025-12-08 17:45:58 +0900

    [FEAT] 즐겨찾기 추가 / 제거 컨트롤러 생성 (Backend)

---
+++ backend/controllers/favoritesController.js
@@ -0,0 +1,78 @@
+const Performance = require("../models/performance");
+const User = require("../models/user");
+
+// post Favorites
+// POST /favorites
+const postFavorites = async (req, res) => {
+  try {
+    const userId = req.user.userId; // 로그인 미들웨어로부터 전달
+    const performanceData = req.body;
+    const url = performanceData.url;
+
+    if (!url) {
+      return res.status(400).json({ message: "URL is required" });
+    }
+
+    await Performance.findOneAndUpdate({ url }, performanceData, {
+      upsert: true,
+      new: true,
+    });
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (!user.favorites.includes(url)) {
+      user.favorites.push(url);
+      await user.save();
+    }
+
+    res
+      .status(200)
+      .json({ message: "update favorites success!", success: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      code: "SERVER_ERROR",
+      message: "서버 에러",
+    });
+  }
+};
+
+// delete Favorites
+// DELETE /favorites
+const deleteFavorites = async (req, res) => {
+  try {
+    const userId = req.user.userId; // 로그인 미들웨어로부터 전달
+    const url = req.body.url;
+
+    if (!url) {
+      return res.status(400).json({ message: "URL is required" });
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (user.favorites.includes(url)) {
+      user.favorites = user.favorites.filter((item) => item !== url);
+      await user.save();
+    }
+
+    res
+      .status(200)
+      .json({ message: "delete favorites success!", success: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      code: "SERVER_ERROR",
+      message: "서버 에러",
+    });
+  }
+};
+
+module.exports = { postFavorites, deleteFavorites };
