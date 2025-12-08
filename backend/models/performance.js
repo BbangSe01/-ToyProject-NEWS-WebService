b4de9e4df45401baa84b4a90b848c27bd41c891a
bangsehyeon <bangsehyeon7091@gmail.com>
2025-12-08 17:41:44 +0900

    [FEAT] 즐겨찾기 뉴스 model 생성 (Backend)

---
+++ backend/models/performance.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose"); // 몽구스를 가져온다.
+// mongoose.schema -> MongoDB 컬렉션 문서의 구조(필드/타입/옵션)를 정함
+const performanceSchema = mongoose.Schema({
+  author: String,
+  description: String,
+  publishedAt: Date,
+  title: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  urlToImage: {
+    type: String,
+  },
+});
+
+// mongoose.model() -> 스키마로 실제 모델(컬렉션 조작 객체)을 만드는 명령어
+// 모델을 통해 db에 저장/조회/삭제 등을 수행함.
+module.exports = mongoose.model("Performance", performanceSchema);
