00c61a02d776cea88b1b3185c8cd491170b71157
bangsehyeon <tpwnstpgus1@naver.com>
2025-12-10 14:13:30 +0900

    [FEAT] 즐겨찾기 API 메서드 정의

--- src/apis/FavoritesApis.ts
+++ src/apis/FavoritesApis.ts
@@ -1,7 +1,30 @@
 import { backendInstance } from "./instance/TokenHttp";
-
-export const GetFavorites = async () => {
+import type { NewsType } from "../types";
+export const getFavorites = async () => {
   const res = await backendInstance.get("/favorites", {
+    timeout: 20000,
+  });
+  return res;
+};
+
+export const postFavRequest = async (
+  sendData: Omit<NewsType, "content" | "source">
+) => {
+  const res = await backendInstance.post(
+    "/favorites",
+    {
+      ...sendData,
+    },
+    {
+      timeout: 20000,
+    }
+  );
+  return res;
+};
+
+export const deleteFavRequest = async (url: string) => {
+  const res = await backendInstance.delete("/favorites", {
+    data: { url },
     timeout: 20000,
   });
   return res;
