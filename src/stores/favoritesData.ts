d7aa14041cedaaf824ede1b1aaa09456c6e4b968
bangsehyeon <tpwnstpgus1@naver.com>
2025-12-10 14:14:03 +0900

    [FEAT] 즐겨찾기 로직 store의 action에서 처리

--- src/stores/favoritesData.ts
+++ src/stores/favoritesData.ts
@@ -1,7 +1,8 @@
 import { defineStore } from "pinia";
 import { ref, computed } from "vue";
 import type { NewsType } from "../types/newsType";
-
+import { postFavRequest, deleteFavRequest } from "../apis/FavoritesApis";
+import { warningToast } from "../utils/warningtoast";
 export const useFavoritesDataStore = defineStore("favoritesData", () => {
   const favoritesData = ref<Omit<NewsType, "content" | "source">[]>([]);
   const favUrlList = computed(() => favoritesData.value.map((x) => x.url));
@@ -12,16 +13,41 @@ export const useFavoritesDataStore = defineStore("favoritesData", () => {
   ) => {
     favoritesData.value = newData;
   };
-  const addFavorites = (newData: Omit<NewsType, "content" | "source">) => {
-    favoritesData.value = [...favoritesData.value, newData];
+
+  const postFavorites = async (newsData: NewsType) => {
+    try {
+      const { content, source, ...sendData } = newsData;
+      await postFavRequest(sendData);
+      favoritesData.value = [...favoritesData.value, sendData];
+      return true;
+    } catch (err) {
+      console.error(err);
+      warningToast("Failed to register favorites!");
+      return false;
+    }
   };
-  const deleteFavorites = (newData: Omit<NewsType, "content" | "source">) => {};
+
+  const deleteFavorites = async (url: string) => {
+    try {
+      await deleteFavRequest(url);
+      const idx = favoritesData.value.findIndex((x) => x.url === url);
+      if (idx !== -1) {
+        favoritesData.value.splice(idx, 1);
+      }
+      return true;
+    } catch (err) {
+      console.error(err);
+      warningToast("Failed to delete favorites!");
+      return false;
+    }
+  };
 
   return {
     favoritesData,
     favUrlList,
     isLoaded,
     setFavoritesData,
-    addFavorites,
+    postFavorites,
+    deleteFavorites,
   };
 });
