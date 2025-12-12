e6ef858203d2fa1982fdb1bc6a404ea5a4f6b6ba
bangsehyeon <tpwnstpgus1@naver.com>
2025-12-12 18:07:20 +0900

    [FEAT] accessToken 유효기간 15분 설정 (Backend)

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const JWT_KEY = process.env.JWT_SECRET;
 const makeToken = (Object) => {
-  const token = jwt.sign(Object, JWT_KEY, { expiresIn: "1h" });
+  const token = jwt.sign(Object, JWT_KEY, { expiresIn: "15m" });
   return token;
 };
 
@@ -10,7 +10,7 @@ const makeRefreshToken = () => {
   // refresh token 발급
   const refreshToken = jwt.sign({}, JWT_KEY, {
     algorithm: "HS256",
-    expiresIn: "14d",
+    expiresIn: "7d",
   });
   return refreshToken;
 };
