b8f094ff3c44885095bf7768d7c4d33aff5e9936
bangsehyeon <tpwnstpgus1@naver.com>
2025-12-15 16:02:27 +0900

    [REFACTOR] 몽고DB의 _id 고유성 해치지 않도록 수정 (Backend)

--- backend/services/tokenService.js
+++ backend/services/tokenService.js
@@ -1,15 +1,15 @@
 const Token = require("../models/token");
 
 const findToken = async (userId) => {
-  const userToken = await Token.findOne({ _id: userId });
+  const userToken = await Token.findOne({ user_id: userId });
   return userToken;
 };
 
 // 생성한 refresh토큰을 mongoDB에 저장하기 위한 코드
-const updateRefresh = async ({ _id, refreshToken }) => {
+const updateRefresh = async ({ user_id, refreshToken }) => {
   const update = await Token.updateOne(
-    { _id },
-    { _id, refreshToken },
+    { user_id: user_id }, // 조건
+    { refreshToken }, // 업데이트 내용
     { upsert: true }
   );
   return update;
